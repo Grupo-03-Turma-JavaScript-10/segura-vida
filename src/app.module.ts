9c3943756fced0144a281f9ad9dae54b2c76cf19
andreyrsy <contatomainort@gmail.com>
2025-12-12 10:56:07 -0300

    chore: dependencias: typeorm, validator & initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from './usuario/app.usuario.module';
+import { EnderecoModule } from './endereco/app.endereco.module';
+import { ContatoEmergenciaModule } from './contatoEmergencia/app.contatoEmergencia.module';
+import { ContatoEmergencia } from './contatoEmergencia/entities/contatoEmergencia.entity';
+import { Endereco } from './endereco/entities/endereco.entity';
+import { Usuario } from './usuario/entities/usuario.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_segura_vida',
+      entities: [ContatoEmergencia, Endereco, Usuario],
+      synchronize: true,
+    }),
+    UsuarioModule,
+    EnderecoModule,
+    ContatoEmergenciaModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
