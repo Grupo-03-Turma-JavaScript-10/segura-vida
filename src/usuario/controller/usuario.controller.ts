5fc3baec00b550f1f787d8eec4e660703568de84
Nicolas Donada <nicolasdonada4@gmail.com>
2025-12-12 11:44:46 -0300

    feat: criado controller usuario

---
+++ src/usuario/controller/usuario.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Get, Post, Put, Delete, Param, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { UsuarioService } from '../services/usuario.service';
+import { Usuario } from '../entities/usuario.entity';
+
+@Controller('/usuarios')
+export class UsuarioController {
+    
+    constructor(private readonly usuarioService: UsuarioService) {}
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Usuario[]> {
+        return this.usuarioService.findAll();
+    }
+
+    @Get('/nome/:nome')
+    @HttpCode(HttpStatus.OK)
+    findByNome(@Param('nome') nome: string): Promise<Usuario[]> {
+        return this.usuarioService.findByNome(nome);
+    }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id') id: number): Promise<Usuario> {
+        return this.usuarioService.findById(id);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() usuario: Usuario): Promise<Usuario> {
+        return this.usuarioService.create(usuario);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() usuario: Usuario): Promise<Usuario> {
+        return this.usuarioService.update(usuario);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id') id: number) {
+        return this.usuarioService.delete(id);
+    }
+}
