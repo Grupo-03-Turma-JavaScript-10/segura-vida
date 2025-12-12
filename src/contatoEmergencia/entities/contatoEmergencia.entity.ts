247d02483f77118b21f779d67272c191dcb3fc56
andreyrsy <contatomainort@gmail.com>
2025-12-12 11:03:36 -0300

    feat: entidades com validator

--- src/contatoEmergencia/entities/contatoEmergencia.entity.ts
+++ src/contatoEmergencia/entities/contatoEmergencia.entity.ts
@@ -1,17 +1,21 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToOne } from 'typeorm';
 import { Usuario } from '../../usuario/entities/usuario.entity';
+import { IsNotEmpty } from 'class-validator';
 
 @Entity({ name: 'tb_contatos_emergencia' })
 export class ContatoEmergencia {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @IsNotEmpty()
     @Column({ length: 100, nullable: false })
     nome: string;
 
+    @IsNotEmpty()
     @Column({ nullable: false })
     telefone: number;
 
+    @IsNotEmpty()
     @Column({ length: 50, nullable: false })
     grauParentesco: string;
 
--- src/endereco/entities/endereco.entity.ts
+++ src/endereco/entities/endereco.entity.ts
@@ -1,20 +1,25 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToOne } from 'typeorm';
 import { Usuario } from '../../usuario/entities/usuario.entity';
+import { IsNotEmpty } from 'class-validator';
 
 @Entity({ name: 'tb_enderecos' })
 export class Endereco {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @IsNotEmpty()
     @Column({ length: 255, nullable: false })
     rua: string;
 
+    @IsNotEmpty()
     @Column({ length: 20, nullable: false })
     numero: string;
 
+    @IsNotEmpty()
     @Column({ length: 100, nullable: false })
     cidade: string;
 
+    @IsNotEmpty()
     @Column({ length: 9, nullable: false })
     cep: string;
 
