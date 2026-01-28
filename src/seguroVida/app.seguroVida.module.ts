0283377c14a42cf34737b8772aaca9bf7a837a4d
MarcosCRosa <marcosluiz4012@gmail.com>
2026-01-28 14:00:43 -0300

    deploy

--- src/seguroVida/app.seguroVida.module.ts
+++ src/seguroVida/app.seguroVida.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { SeguroVidaController } from "./controller/seguroVida.controller";
 import { SeguroVidaService } from "./services/seguroVida.service";
 import { SeguroVida } from "./entities/seguroVida.entity";
+import { Usuario } from "../usuario/entities/usuario.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([SeguroVida])],
+    imports: [TypeOrmModule.forFeature([SeguroVida, Usuario])],
     controllers: [SeguroVidaController],
     providers: [SeguroVidaService],
     exports: [SeguroVidaService]
